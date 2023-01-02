efc18fa802f4628004156f6158230dc6b0e25e72
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-03 00:11:34 +0100

    add detail page

---
+++ lib/src/features/pages/detail_page.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class Detail extends StatelessWidget {
+//   Detail({this.name, this.image, this.description});
+//   final String name;
+//   final String image;
+//   final String description;
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       children: [
+//         Container(
+//           height: 240.0,
+//           child: Hero(tag: name, child: Material(child: Image.asset(image))),
+//         )
+//       ],
+//     );
+//   }
+// }
